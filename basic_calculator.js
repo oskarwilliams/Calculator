157d4da465a59d643a3c95ea578f7da78006852e
oskarwilliam378 <oskarwilliam378@gmail.com>
2019-02-09 23:35:03 +0000

    Added basic calculator

---
+++ basic_calculator.js
@@ -0,0 +1,63 @@
+const readline = require('readline-sync');
+
+var answer
+
+function multiply(a,b) {
+    var y;
+    return y = a*b;
+}
+
+function add(a,b) {
+    var y;
+    return y = Number(a) + Number(b);
+}
+
+function subtract(a,b) {
+    var y;
+    return y = a-b;
+}
+
+function divide(a,b) {
+    var y;
+    return y = a/b;
+}
+
+console.log('Please enter the operator:');
+const oper = readline.prompt();
+
+console.log('Please enter the first number:');
+const x1 = readline.prompt();
+
+console.log('Please enter the second number:');
+const x2 = readline.prompt();
+
+
+// if (oper == '+') {
+//     answer = add(x1,x2);
+// } else if (oper == '-') {
+//     answer = subtract(x1,x2);
+// } else if (oper == '*') {
+//     answer = multiply(x1,x2);
+// } else if (oper == '/') {
+//     answer = divide(x1,x2);
+// }
+
+switch(oper) {
+    case '+':
+        answer = add(x1,x2);
+        break;
+    case '-':
+        answer = subtract(x1,x2);
+        break;        
+    case '*':
+        answer = multiply(x1,x2);
+        break;
+    case '/':
+        answer = divide(x1,x2);
+        break;
+
+}
+
+
+
+console.log(answer);
--- multiply.js
+++ multiply.js
@@ -1,15 +1,15 @@
 const readline = require('readline-sync');
 
+function multiply(a,b) {
+    var y;
+    return y = a*b;
+}
+
 console.log('Please enter the first value to multiply:');
 const x1 = readline.prompt();
 
 console.log('Please enter the second value to multiply:');
 const x2 = readline.prompt();
-
-function multiply(a,b) {
-    var y;
-    return y = a*b;
-}
 
 console.log(multiply(x1,x2));
 
